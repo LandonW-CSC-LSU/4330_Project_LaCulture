3c02f87f4fec319805e2e17c38a564f1ce8135a4
AllinOne000 <alabra4@lsu.edu>
2025-09-05 11:17:29 -0500

    Working Out Bugs

--- LaCulture_API/obj/Debug/net9.0/LaCulture.API.AssemblyInfo.cs
+++ LaCulture_API/obj/Debug/net9.0/LaCulture.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LaCulture.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+913485e00062a5dbefadf484abb01aa3ae4571fc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9ebb75e652c4517a888887df1837c1f66b42eb2f")]
 [assembly: System.Reflection.AssemblyProductAttribute("LaCulture.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LaCulture.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
