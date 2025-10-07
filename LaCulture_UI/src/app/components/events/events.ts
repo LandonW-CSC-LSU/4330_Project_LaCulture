d2e760205944835bd281158516ef43b24a1f4cb9
NicholasLuke <nluke2@lsu.edu>
2025-10-07 08:18:50 -0500

    more events

--- LaCulture_UI/src/app/components/events/events.ts
+++ LaCulture_UI/src/app/components/events/events.ts
@@ -27,7 +27,15 @@ export class EventsComponent {
     { id: 7, title: 'Nola Funk Fest 2025', date: '10/17-19/2025', location: 'New Orleans', website: 'https://www.nolafunkfest.com/' },
     { id: 8, title: 'Praise Fest', date: '10/17-19/2025', location: 'New Orleans', website: 'https://www.praisefestnola.com/' },
     { id: 9, title: 'New Orleans Film Festival ', date: '10/23/2025-11/2/2025', location: 'New Orleans', website: 'https://neworleansfilmsociety.org/attend/' },
-    { id: 10, title: 'LGBT Halloween New Orleans (HNO)', date: '10/24-26/2025', location: 'New Orleans', website: 'https://www.halloweenneworleans.com/' }
+    { id: 10, title: 'LGBT Halloween New Orleans (HNO)', date: '10/24-26/2025', location: 'New Orleans', website: 'https://www.halloweenneworleans.com/' },
+    { id: 11, title: 'NOLA Reggae Fest', date: '10/24-26/2025', location: 'New Orleans', website: 'https://www.eventbrite.com/e/2025-nola-reggae-fest-tickets-1369000991819' },
+    { id: 12, title: 'Krewe of BOO!', date: '10/25/2025', location: 'New Orleans', website: 'https://www.kreweofboo.com/' },
+    { id: 13, title: 'Treme Fall Fest', date: '10/25/2025', location: 'New Orleans', website: 'https://www.tremefest.org/' },
+    { id: 14, title: 'NOLA MusiCon', date: '10/28-30/2025', location: 'New Orleans', website: 'https://www.nolamusicon.com/' },
+    { id: 15, title: 'Bayou Bacchanal', date: '10/31-11/1/2025', location: 'New Orleans', website: 'https://www.friendsofculture.org/' },
+    { id: 16, title: 'Freret Street Fall Festival', date: '11/1/2025', location: 'New Orleans', website: 'https://freretstreetfestival.org/' },
+    { id: 17, title: 'Tremé Creole Gumbo Festival', date: '11/8-9/2025', location: 'New Orleans', website: 'https://www.jazzandheritage.org/events/2025-treme-creole-gumbo-festival/' },
+    { id: 18, title: 'Beignet Fest', date: '11/15/2025', location: 'New Orleans', website: 'https://beignetfest.com/' }
   ]);
 
    public locations = computed(() => ['All', ...new Set(this.items().map(e => e.location))]);
