09f545de9c4b009003dfd2a637f043e08ee9757d
LandonW-CSC-LSU <lwalls6@lsu.edu>
2025-10-09 09:29:52 -0500

    merge conflict fixes

--- LaCulture_UI/src/app/components/home/home.component.ts
+++ LaCulture_UI/src/app/components/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 import {
   CarouselComponent,
   CarouselConfig,
@@ -29,6 +30,7 @@ export class CarouselCustomConfig {
   imports: [
     CommonModule,
     FormsModule,
+    RouterModule,
     CarouselComponent,
     CarouselInnerComponent,
     CarouselItemComponent,
