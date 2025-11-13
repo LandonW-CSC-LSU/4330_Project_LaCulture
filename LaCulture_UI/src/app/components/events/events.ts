8b17b57cc55b26ebfa97b9afe9d5d84775e9c39d
NicholasLuke <nluke2@lsu.edu>
2025-11-13 11:07:59 -0600

    change to events ui

--- LaCulture_UI/src/app/components/events/events.ts
+++ LaCulture_UI/src/app/components/events/events.ts
@@ -1,5 +1,10 @@
 import { ChangeDetectionStrategy, Component, computed, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 interface Event {
   id: number;
@@ -7,22 +12,31 @@ interface Event {
   date: string;
   location: string;
   website?: string;
+  image?: string;
 }
 
 @Component({
   selector: 'app-events',
-  imports: [CommonModule],
-  templateUrl: './events.html', 
+  standalone: true,
+  imports: [
+    CommonModule,
+    MatCardModule,
+    MatButtonModule,
+    MatIconModule,
+    MatProgressSpinnerModule,
+    MatTooltipModule
+  ],
+  templateUrl: './events.html',
   styleUrls: ['./events.css'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class EventsComponent {
   public items = signal<Event[]>([
-    { id: 1, title: 'LSU vs Texas A&M', date: '10/25/2025', location: 'Baton Rouge', website: 'https://lsusports.evenue.net/list/FB'},
-    { id: 2, title: 'Saints vs Buccaneers', date: '10/26/2025', location: 'New Orleans', website: 'https://www.neworleanssaints.com/tickets/' },
-    { id: 3, title: 'National Fried Chicken Festival', date: '10/4-5/2025', location: 'New Orleans', website: 'https://www.friedchickenfestival.com/' },
-    { id: 4, title: 'Oktoberfest', date: '10/10-25/2025', location: 'New Orleans', website: 'https://deutscheshaus.org/oktoberfest/' },
-    { id: 5, title: 'Nola Funk Fest 2025', date: '10/17-19/2025', location: 'New Orleans', website: 'https://www.nolafunkfest.com/' },
+    { id: 1, title: 'LSU vs Texas A&M', date: '10/25/2025', location: 'Baton Rouge', website: 'https://lsusports.evenue.net/list/FB', image: 'assets/images/tigerstadium.jpg'},
+    { id: 2, title: 'Saints vs Buccaneers', date: '10/26/2025', location: 'New Orleans', website: 'https://www.neworleanssaints.com/tickets/', image: 'assets/images/superdome.jpg' },
+    { id: 3, title: 'National Fried Chicken Festival', date: '10/4-5/2025', location: 'New Orleans', website: 'https://www.friedchickenfestival.com/', image: 'assets/images/crawfish.jpg' },
+    { id: 4, title: 'Oktoberfest', date: '10/10-25/2025', location: 'New Orleans', website: 'https://deutscheshaus.org/oktoberfest/', image: 'assets/images/mardiGras.png' },
+    { id: 5, title: 'Nola Funk Fest 2025', date: '10/17-19/2025', location: 'New Orleans', website: 'https://www.nolafunkfest.com/', image: 'assets/images/mardiGrasFloat.jpg' },
     { id: 6, title: 'Praise Fest', date: '10/17-19/2025', location: 'New Orleans', website: 'https://www.praisefestnola.com/' },
     { id: 7, title: 'New Orleans Film Festival ', date: '10/23/2025-11/2/2025', location: 'New Orleans', website: 'https://neworleansfilmsociety.org/attend/' },
     { id: 8, title: 'LGBT Halloween New Orleans (HNO)', date: '10/24-26/2025', location: 'New Orleans', website: 'https://www.halloweenneworleans.com/' },
@@ -50,5 +64,10 @@ export class EventsComponent {
 
   public filterByLocation(location: string): void {
     this.selectedLocation.set(location);
+  }
+
+  // trackBy for the event list to avoid unnecessary DOM updates
+  public trackByEventId(index: number, event: Event): number {
+    return event.id;
   }
 }
